794d4bec728ea8398c4d8a18d7eda5a670efb3ca
burcakulug <burcak.ulug@feisystems.com>
2016-03-18 12:52:30 -0400

    Create initial edge server application based on Spring Cloud and Netflix Zuul
    #8417:As a developer, I can secure and discover MHC microservices

---
+++ edge-server/src/test/java/gov/samhsa/mhc/edgeserver/EdgeServerApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.samhsa.mhc.edgeserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EdgeServerApplication.class)
+public class EdgeServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
