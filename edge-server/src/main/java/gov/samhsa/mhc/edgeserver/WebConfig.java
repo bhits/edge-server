d5b515a34a251624c451c3ced1f9abd806262ab0
burcakulug <burcak.ulug@feisystems.com>
2016-03-18 17:40:54 -0400

    Add https redirection configuration to edge server
    #8417:As a developer, I can secure and discover MHC microservices

---
+++ edge-server/src/main/java/gov/samhsa/mhc/edgeserver/WebConfig.java
@@ -0,0 +1,49 @@
+package gov.samhsa.mhc.edgeserver;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Value("${server.port}")
+    public int redirectPort;
+
+    @Value("${eureka.instance.non-secure-port}")
+    public int nonSecurePort;
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(nonSecurePort);
+        connector.setSecure(false);
+        connector.setRedirectPort(redirectPort);
+
+        return connector;
+    }
+}
