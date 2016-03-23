435e74f8dd09bfe611a1c90a025b879a5321d509
burcakulug <burcak.ulug@feisystems.com>
2016-03-23 15:00:38 -0400

    Add config support for root redirection, enable healthcheck and initial routes for uaa and pp-ui
    #8417:As a developer, I can secure and discover MHC microservices

--- edge-server/src/main/java/gov/samhsa/mhc/edgeserver/EdgeServerApplication.java
+++ edge-server/src/main/java/gov/samhsa/mhc/edgeserver/EdgeServerApplication.java
@@ -1,12 +1,24 @@
 package gov.samhsa.mhc.edgeserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableZuulProxy
+@Controller
 public class EdgeServerApplication {
+
+    @Value("${mhc.edge-server.root-redirect-url}")
+    private String rootRedirectUrl;
+
+    @RequestMapping("/")
+    public String rootRedirection() {
+        return rootRedirectUrl;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EdgeServerApplication.class, args);
