03bff127286c50a84bfd419997a991e9ce9c8791
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2016-06-17 19:29:12 -0400

    Update spring-boot-starter-parent & spring.cloud dependency versions
    Details:
       - Update spring-boot-start-parent from version 1.3.3.RELEASE to 1.3.5.RELEASE
     - Update spring.cloud.version property from Brixton.M5 to Brixton.RELEASE
     - Add explicit sensitiveHeaders propert in application.yml for each Zuul route to only filter Cookie and Set-Cookie fields and not the Authorization field
     - Add '@WebIntegrationTest' annotation to EdgeServerApplicationTests class to fix test failures following spring version upgrade
    
    #9219 - As a provider, I will be able to utilize a feature to audit a patient consent when signed or revoked

--- edge-server/src/test/java/gov/samhsa/mhc/edgeserver/EdgeServerApplicationTests.java
+++ edge-server/src/test/java/gov/samhsa/mhc/edgeserver/EdgeServerApplicationTests.java
@@ -3,10 +3,12 @@ package gov.samhsa.mhc.edgeserver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = EdgeServerApplication.class)
+@WebIntegrationTest
 public class EdgeServerApplicationTests {
 
 	@Test
