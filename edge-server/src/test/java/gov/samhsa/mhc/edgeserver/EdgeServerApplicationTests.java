8d33059227d79e044beac3a4899c870a0d975eda
burcakulug <burcak.ulug@feisystems.com>
2016-06-21 10:58:09 -0400

    Add random port configuration and remove ssl profile from tests to stabilize the build
    #9219:As a provider, I will be able to utilize a feature to audit a patient consent when signed or revoked

--- edge-server/src/test/java/gov/samhsa/mhc/edgeserver/EdgeServerApplicationTests.java
+++ edge-server/src/test/java/gov/samhsa/mhc/edgeserver/EdgeServerApplicationTests.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = EdgeServerApplication.class)
-@WebIntegrationTest
+@WebIntegrationTest({"server.port=0", "management.port=0"})
+@ActiveProfiles("eureka-ssl")
 public class EdgeServerApplicationTests {
 
 	@Test
